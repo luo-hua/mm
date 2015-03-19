c3370df1c29b8c6a52e085c138869eab23dc4e16
martin <jspine@126.com>
2015-03-19 15:11:06 +0800

    新建

---
+++ mm/src/main/java/net/itbrother/mm/App.java
@@ -0,0 +1,13 @@
+package net.itbrother.mm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
